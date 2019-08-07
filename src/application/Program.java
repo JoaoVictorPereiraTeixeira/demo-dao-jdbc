948cb66347d4b9cda762c85797f9d4ff3552fb68
JoaoVictorPereiraTeixeira <joaovictorteixeira9819@gmail.com>
2019-08-07 11:04:47 -0300

    Delete implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,13 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
+
 
 public class Program {
 
@@ -33,6 +35,17 @@ public class Program {
 		for(Seller obj : list) {
 			System.out.println(obj);
 		}
+		
+		System.out.println("\n=== TEST 4: seller insert ===");
+		
+		Seller newSeller = new Seller(null,"Gred","gred@hotmail.com",new Date(), 4000.0, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New id = " + newSeller.getId());
+		
+		
+		
+		
+
 	}
 
 }
